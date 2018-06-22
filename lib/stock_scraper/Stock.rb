ab3a8f004f9220a1262d64fc47a1ff43defe2178
Scott Weinstein <sweinstein27@gmail.com>
2018-06-22 15:48:22 +0000

    Done.

--- lib/stock_scraper/Stock.rb
+++ lib/stock_scraper/Stock.rb
@@ -1,6 +1,6 @@
 class StockScraper::Stock
 
-  attr_accessor :name, :stock_price, :volume, :pe, :url
+  attr_accessor :stock_name, :stock_price, :volume, :pe, :url, :stock_symbol
 
   def initialize(name = nil, url = nil )
     @name = name
@@ -25,48 +25,13 @@ class StockScraper::Stock
 def self.scrape_stock_movers_top
      doc = Nokogiri::HTML(open('https://money.cnn.com/data/us_markets/'))
      stocks = []
-     doc.css("tr").each do |row|
-       binding.pry
-       row.css("td")
-        #  symbol = stock.css(".symbol")
-        #  link = "#{stock.attr('href')}"
-        #  name =
-        #  percent_change =
+     doc.css("td").each do |row|
+       stock_symbol = row.css(".wsod_symbol").children.text
+       stock_name = row.css("span").text
+       url = row.css(".wsod_symbol").attribute("href").value
+        stocks << {stock_symbol: stock_symbol, stock_name: stock_name, url: url  }
      end
    end
-
-# def self.scrape_stock_movers
-#      doc = Nokogiri::HTML(open('https://www.cnbc.com'))
-#      doc.css('market-movers')
-# end
-
-
-
-
-
-
-
-
-  # def self.movers
-  #   attr_accessor :name, :stock_price, :volume, :pe
-  #   #returns all instances of stock from movers scraper
-  #   stock_1 = self.new
-  #   stock_1.name = "stockname1"
-  #   stock_1.stock_price = "$45"
-  #   stock_1.volume = "55m"
-  #   stock_1.pe = "15"
-  # end
-
-def self.movers
-  attr_accessor :name, :stock_price, :volume, :pe
-  #returns all instances of stock from movers scraper
-  stock_1 = self.new
-  stock_1.name = "stockname1"
-  stock_1.stock_price = "$45"
-  stock_1.volume = "55m"
-  stock_1.pe = "15"
-
-end
 
 
 
